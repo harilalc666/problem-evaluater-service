3a155d910b2022545779595b2d208f835734bb7d
harilal.chauhan <harilal.chauhan>
2024-09-03 01:19:11 +0530

    added basic routes in the project

---
+++ src/routes/v1/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+
+const router = express.Router();
+
+const currentDate: Date = new Date();
+
+router.get('/ping', (req: Request, res: Response)=>{
+  // const data = req.params;
+  res.status(200).json({
+    sucess: true,
+    data: "",
+    message: `Health API is working ${currentDate}`
+  })
+})
+
+export default router;
